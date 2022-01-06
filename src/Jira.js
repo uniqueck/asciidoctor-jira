7fd060c751a8496b68794d246f55f763a49d390f
Constantin Krüger <Constantinidis@web.de>
2022-01-06 22:32:38 +0100

    add issueType icon to table

--- src/Jira.js
+++ src/Jira.js
@@ -1,9 +1,55 @@
 
 const request = require('sync-request')
+const path = require('path')
 
 class Jira {
   constructor (doc) {
     this.doc = doc
+
+    const jiraUserName = this.doc.getAttribute('jira-username') || process.env.AJE_USERNAME
+    const jiraApiToken = this.doc.getAttribute('jira-apitoken') || process.env.AJE_APITOKEN || process.env.AJE_PASSWORD
+    const auth = 'Basic ' + Buffer.from(jiraUserName + ':' + jiraApiToken).toString('base64')
+    this.headers = {
+      authorization: auth
+    }
+  }
+
+  getDirPath () {
+    const imagesOutputDir = this.doc.getAttribute('imagesoutdir')
+    const outDir = this.doc.getAttribute('outdir')
+    const toDir = this.doc.getAttribute('to_dir')
+    const baseDir = this.doc.getBaseDir()
+    const imagesDir = this.doc.getAttribute('imagesdir') || ''
+    let dirPath
+    if (imagesOutputDir) {
+      dirPath = imagesOutputDir
+    } else if (outDir) {
+      dirPath = path.join(outDir, imagesDir)
+    } else if (toDir) {
+      dirPath = path.join(toDir, imagesDir)
+    } else {
+      dirPath = path.join(baseDir, imagesDir)
+    }
+    return dirPath
+  }
+
+  download (imageName, imageUrl, vfs) {
+    const exists = typeof vfs !== 'undefined' && typeof vfs.exists === 'function' ? vfs.exists : require('./node-fs.js').exists
+    const read = typeof vfs !== 'undefined' && typeof vfs.read === 'function' ? vfs.read : require('./node-fs.js').read
+    const add = typeof vfs !== 'undefined' && typeof vfs.add === 'function' ? vfs.add : require('./node-fs.js').add
+
+    const dirPath = this.getDirPath()
+    const filePath = path.format({ dir: dirPath, base: imageName })
+    const encoding = 'binary'
+    const mediaType = 'image/svg+xml'
+
+    const contents = exists(filePath) ? read(filePath, encoding) : request('GET', imageUrl, { headers: this.headers }).getBody('utf-8')
+    add({
+      relative: dirPath,
+      basename: imageName,
+      mediaType: mediaType,
+      contents: Buffer.from(contents, encoding)
+    })
   }
 
   searchIssues (jql) {
@@ -12,14 +58,8 @@ class Jira {
     try {
       const jiraBaseUrl = this.doc.getAttribute('jira-host') || process.env.AJE_JIRABASEURL
       const jiraRestApiSearchEndpoint = jiraBaseUrl + '/rest/api/2/search'
-      const jiraUserName = this.doc.getAttribute('jira-username') || process.env.AJE_USERNAME
-      const jiraApiToken = this.doc.getAttribute('jira-apitoken') || process.env.AJE_APITOKEN || process.env.AJE_PASSWORD
-      const auth = 'Basic ' + Buffer.from(jiraUserName + ':' + jiraApiToken).toString('base64')
-      const headers = {
-        authorization: auth
-      }
       const res = request('GET', jiraRestApiSearchEndpoint, {
-        headers: headers,
+        headers: this.headers,
         qs: data
       })
       issues = JSON.parse(res.getBody('utf-8')).issues
@@ -36,14 +76,8 @@ class Jira {
     try {
       const jiraBaseUrl = this.doc.getAttribute('jira-host') || process.env.AJE_JIRABASEURL
       const jiraRestApiSearchEndpoint = jiraBaseUrl + '/rest/api/2/search'
-      const jiraUserName = this.doc.getAttribute('jira-username') || process.env.AJE_USERNAME
-      const jiraApiToken = this.doc.getAttribute('jira-apitoken') || process.env.AJE_APITOKEN || process.env.AJE_PASSWORD
-      const auth = 'Basic ' + Buffer.from(jiraUserName + ':' + jiraApiToken).toString('base64')
-      const headers = {
-        authorization: auth
-      }
       const res = request('GET', jiraRestApiSearchEndpoint, {
-        headers: headers,
+        headers: this.headers,
         qs: data
       })
       result = JSON.parse(res.getBody('utf-8')).issues[0]
--- test/antora/test.spec.js
+++ test/antora/test.spec.js
@@ -2,19 +2,124 @@
 const rimrafSync = require('rimraf')
 const cheerio = require('cheerio')
 const fs = require('fs')
+const existsFile = fs.existsSync
 const chai = require('chai')
 const expect = chai.expect
 
 const generateSite = require('@antora/site-generator-default')
 
 describe('Antora integration', () => {
-  beforeEach(() => {
+  before(async function () {
+    this.timeout(50000)
     rimrafSync(`${__dirname}/public`, function (error) {})
-  })
-  it('should generate a site with jira table', async () => {
     await generateSite([`--playbook=${__dirname}/site.yml`])
-    const $ = cheerio.load(fs.readFileSync(`${__dirname}/public/antora-jira/index.html`))
-    // const imageElements = $('table')
-    // expect(imageElements.length).to.equal(1)
-  }).timeout(50000)
+  })
+  it('default jql', async () => {
+    const $ = cheerio.load(fs.readFileSync(`${__dirname}/public/antora-jira/blockmacro.html`))
+    const tableElement = $('h3[id="_defaults"]').parent().find('table')
+    expect(tableElement).to.not.be.null
+    const thElements = $(tableElement).find('thead tr th')
+    expect(thElements).to.not.be.null
+    expect(thElements.length).to.equal(5)
+
+    // check column header ID
+    let thElement = thElements.get(0)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('ID')
+
+    // check column header Priority
+    thElement = thElements.get(1)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Priority')
+
+    // check column header Created
+    thElement = thElements.get(2)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Created')
+
+    // check column header Assignee
+    thElement = thElements.get(3)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Assignee')
+
+    // check column header Summary
+    thElement = thElements.get(4)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Summary')
+
+    // select table rows with content
+    const trElements = $(tableElement).find('tbody tr')
+    expect(trElements).to.not.be.null
+    expect(trElements.length).to.equal(5)
+
+  })
+  it('define a custom jql', async () => {
+    const $ = cheerio.load(fs.readFileSync(`${__dirname}/public/antora-jira/blockmacro.html`))
+    const tableElement = $('h3[id="_custom_jql"]').parent().find('table')
+    expect(tableElement).to.not.be.null
+    expect($(tableElement).find('caption').text()).to.equal('Table 1. custom')
+    const thElements = $(tableElement).find('thead tr th')
+    expect(thElements).to.not.be.null
+    expect(thElements.length).to.equal(5)
+    // check column header ID
+    let thElement = thElements.get(0)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('ID')
+
+    // check column header Priority
+    thElement = thElements.get(1)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Priority')
+
+    // check column header Created
+    thElement = thElements.get(2)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Created')
+
+    // check column header Assignee
+    thElement = thElements.get(3)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Assignee')
+
+    // check column header Summary
+    thElement = thElements.get(4)
+    expect(thElement).to.not.be.null
+    expect($(thElement).text()).to.equal('Summary')
+
+    // select table rows with content
+    const trElements = $(tableElement).find('tbody tr')
+    expect(trElements).to.not.be.null
+    expect(trElements.length).to.equal(1)
+
+    let trElement = trElements.get(0)
+    expect(trElement).to.not.be.null
+    let tdElements = $(trElement).find('td')
+    expect(tdElements).to.not.be.null
+    expect(tdElements.length).to.equal(5)
+
+    // column id
+    let tdElement = tdElements.get(0)
+    let imageElement = $(tdElement).find('div div p span img')
+    expect(imageElement).to.not.be.null
+    expect($(imageElement).attr('src')).to.equal('_images/jira-issuetype-story.svg')
+    expect(existsFile(`${__dirname}/public/antora-jira/_images/jira-issuetype-story.svg`))
+    expect($($(tdElement).find('div div p a')).attr('href')).to.equal('https://uniqueck.atlassian.net/browse/DOC-1')
+
+    // column priority
+    tdElement = tdElements.get(1)
+    expect($(tdElement).text()).to.equal('Medium')
+
+    // column created
+    tdElement = tdElements.get(2)
+    expect($(tdElement).text()).to.equal('2021-08-08T12:13:19.203+0200')
+
+    // column assignee
+    tdElement = tdElements.get(3)
+    expect($(tdElement).text()).to.equal('Constantin Krüger')
+
+    // column summary
+    tdElement = tdElements.get(4)
+    expect($(tdElement).text()).to.equal('Test123')
+
+  })
 })
